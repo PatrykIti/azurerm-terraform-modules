005c6ef23c481af9008868d60bd314948b069b73
Patryk Ciechanski <patryk.ciechanski@inetum.com>
2025-07-16 21:56:25 +0200

    chore: aks up test fixtures

--- modules/azurerm_kubernetes_cluster/tests/fixtures/negative/main.tf
+++ modules/azurerm_kubernetes_cluster/tests/fixtures/negative/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "test" {
 
 # This should fail due to invalid name
 module "kubernetes_cluster" {
-  source = "../../../.."
+  source = "../../../"
 
   name                = "INVALID-NAME-WITH-UPPERCASE" # Should fail validation
   resource_group_name = azurerm_resource_group.test.name
