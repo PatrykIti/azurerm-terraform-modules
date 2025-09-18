b1a908f788d1bf587f64cfd803adef034fd909a4
Patryk Ciechański <patryk.ciechanski@inetum.com>
2025-09-18 21:06:19 +0200

    chore: up

--- modules/azurerm_virtual_network/.releaserc.js
+++ modules/azurerm_virtual_network/.releaserc.js
@@ -146,7 +146,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
           fi
 
           if command -v terraform-docs &> /dev/null; then
-            terraform-docs markdown table --output-file "modules/${MODULE_NAME}/README.md" --output-mode inject "modules/${MODULE_NAME}"
+            (cd "modules/${MODULE_NAME}" && terraform-docs .)
           fi
 
           if [[ -x "./scripts/update-root-readme.sh" ]]; then
