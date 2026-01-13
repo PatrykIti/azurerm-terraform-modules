a600c746561a661eb6fda4828ac7b8fb8cafac6b
Patryk Ciechanski <patryk.ciechanski@inetum.com>
2026-01-13 22:23:23 +0100

    chore: up fixture assertion

--- modules/azuredevops_group/tests/azuredevops_group_test.go
+++ modules/azuredevops_group/tests/azuredevops_group_test.go
@@ -106,7 +106,7 @@ func TestAzuredevopsGroupValidationRules(t *testing.T) {
 
 	_, err := terraform.InitAndPlanE(t, terraformOptions)
 	require.Error(t, err)
-	assert.Contains(t, err.Error(), "group_memberships.group_descriptor must be a non-empty string")
+	assert.Contains(t, err.Error(), "group_memberships.group_descriptor must be a non-empty string when set.")
 }
 
 // Helper function to get terraform options
