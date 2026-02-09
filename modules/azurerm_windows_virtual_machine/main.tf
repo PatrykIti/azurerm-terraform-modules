0fa120b158f83aeb84ae96116ecf47376d1add83
Patryk Ciechanski <patryk.ciechanski@inetum.com>
2026-02-09 22:43:35 +0100

    chore: up

--- modules/azurerm_windows_virtual_machine/main.tf
+++ modules/azurerm_windows_virtual_machine/main.tf
@@ -3,6 +3,8 @@ locals {
     [var.network.primary_network_interface_id],
     [for id in var.network.network_interface_ids : id if id != var.network.primary_network_interface_id]
   )
+
+  data_disks_by_name = { for disk in var.data_disks : disk.name => disk }
 }
 
 resource "azurerm_windows_virtual_machine" "windows_virtual_machine" {
@@ -187,3 +189,58 @@ resource "azurerm_windows_virtual_machine" "windows_virtual_machine" {
 
   tags = var.tags
 }
+
+resource "azurerm_managed_disk" "managed_disk" {
+  for_each = local.data_disks_by_name
+
+  name                = each.value.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  create_option          = "Empty"
+  storage_account_type   = each.value.storage_account_type
+  disk_size_gb           = each.value.disk_size_gb
+  disk_encryption_set_id = each.value.disk_encryption_set_id
+  zone                   = var.placement.zone
+
+  tags = var.tags
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "virtual_machine_data_disk_attachment" {
+  for_each = local.data_disks_by_name
+
+  managed_disk_id           = azurerm_managed_disk.managed_disk[each.key].id
+  virtual_machine_id        = azurerm_windows_virtual_machine.windows_virtual_machine.id
+  lun                       = each.value.lun
+  caching                   = each.value.caching
+  write_accelerator_enabled = each.value.write_accelerator_enabled
+}
+
+resource "azurerm_virtual_machine_extension" "virtual_machine_extension" {
+  for_each = { for extension in var.extensions : extension.name => extension }
+
+  name                 = each.value.name
+  virtual_machine_id   = azurerm_windows_virtual_machine.windows_virtual_machine.id
+  publisher            = each.value.publisher
+  type                 = each.value.type
+  type_handler_version = each.value.type_handler_version
+
+  auto_upgrade_minor_version  = each.value.auto_upgrade_minor_version
+  automatic_upgrade_enabled   = each.value.automatic_upgrade_enabled
+  failure_suppression_enabled = each.value.failure_suppression_enabled
+  provision_after_extensions  = each.value.provision_after_extensions
+
+  settings           = each.value.settings != null ? jsonencode(each.value.settings) : null
+  protected_settings = each.value.protected_settings != null ? jsonencode(each.value.protected_settings) : null
+
+  dynamic "protected_settings_from_key_vault" {
+    for_each = each.value.protected_settings_from_key_vault != null ? [each.value.protected_settings_from_key_vault] : []
+    content {
+      secret_url      = protected_settings_from_key_vault.value.secret_url
+      source_vault_id = protected_settings_from_key_vault.value.source_vault_id
+    }
+  }
+
+  tags = try(each.value.tags, null)
+}
+
